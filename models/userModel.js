43f91a6d5abd282446bb541ff12e822f10a07160
Bedie-Godsway <bediegodsway@gmail.com>
2026-05-09 02:09:15 +0000

    user models shema  added for authentication

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    }
+   
+});
+
+// Create indexes for unique fields
+userSchema.index({ username: 1, email: 1}, { unique: true });
+
+// Create and export the User model
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
